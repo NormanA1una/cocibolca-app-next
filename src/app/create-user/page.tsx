a7f0627fb85a5730d8d2e3bec47fd0032a24b230
NormanA1una <baciloferoz@gmail.com>
2023-08-23 16:07:24 -0600

    feat: sign in form is done

--- src/app/create-user/page.tsx
+++ src/app/create-user/page.tsx
@@ -1,7 +1,121 @@
+"use client";
+
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+
 export default function CreateUser() {
+  const { register, handleSubmit, reset } = useForm<SignInForm>();
+
+  const onSubmit = (data: SignInForm) => {
+    console.log(data);
+  };
   return (
-    <div className="min-h-screen">
-      <h1>Esto es para la creación de usuarios</h1>
+    <div className="min-h-screen flex items-center">
+      <form
+        className="animate__animated animate__fadeInLeftBig bg-neutral-50 border-opacity-50 rounded p-5 w-full max-w-[700px] mx-auto shadow-sm h-screen md:h-[550px]"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <h1 className="text-center font-bold text-3xl my-8">Crear usuario</h1>
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Correo electrónico
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 "
+            placeholder="name@flowbite.com"
+            required
+            {...register("username")}
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="username"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Nombre de Usuario
+          </label>
+          <input
+            type="text"
+            id="username"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 "
+            placeholder="name example"
+            required
+            {...register("username")}
+          />
+        </div>
+        <div className="mb-6 flex justify-between">
+          <div className="w-full max-w-[160px] md:max-w-[250px]">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Contraseña
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+              {...register("password")}
+            />
+          </div>
+
+          <div className="w-full max-w-[160px] md:max-w-[250px]">
+            <label
+              htmlFor="role"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Rol
+            </label>
+            <select
+              id="password"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+              {...register("role")}
+            >
+              <option>-</option>
+              <option>Administrador</option>
+              <option>Usuario</option>
+            </select>
+          </div>
+        </div>
+        <div className="flex justify-center mb-6">
+          <div className="flex items-center h-5">
+            <input
+              id="remember"
+              type="checkbox"
+              value=""
+              className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+              {...register("remember")}
+            />
+          </div>
+          <label
+            htmlFor="remember"
+            className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            Recordar usuario /{" "}
+            <Link
+              href={"create-user"}
+              className=" hover:text-blue-600 hover:underline"
+            >
+              Crear
+            </Link>
+          </label>
+        </div>
+        <div className=" text-center">
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-[200px] px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Log In
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useForm } from "react-hook-form";
 import "animate.css";
+import Link from "next/link";
 
 export default function Login() {
   const { register, handleSubmit, reset } = useForm<LoginForm>();
@@ -11,15 +12,12 @@ export default function Login() {
   };
 
   return (
-    <div className="min-h-screen">
-      <h1 className="text-center mt-52 mb-10 font-bold text-3xl animate__animated animate__fadeIn">
-        INICIAR SESIÓN
-      </h1>
-
+    <div className="min-h-screen flex items-center">
       <form
-        className="animate__animated animate__fadeInLeftBig bg-slate-400 border-opacity-50 rounded p-5 w-full max-w-[700px] mx-auto"
+        className="animate__animated animate__fadeInLeftBig bg-neutral-50 border-opacity-50 rounded p-5 w-full max-w-[700px] mx-auto shadow-sm h-screen md:h-[450px]"
         onSubmit={handleSubmit(onSubmit)}
       >
+        <h1 className="text-center font-bold text-3xl my-8">Iniciar sesión</h1>
         <div className="mb-6">
           <label
             htmlFor="username"
@@ -31,7 +29,7 @@ export default function Login() {
             type="text"
             id="username"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 "
-            placeholder="name@flowbite.com"
+            placeholder="name example"
             required
             {...register("username")}
           />
@@ -51,7 +49,7 @@ export default function Login() {
             {...register("password")}
           />
         </div>
-        <div className="flex items-start mb-6">
+        <div className="flex justify-center mb-6">
           <div className="flex items-center h-5">
             <input
               id="remember"
@@ -65,15 +63,23 @@ export default function Login() {
             htmlFor="remember"
             className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
           >
-            Recordar usuario
+            Recordar usuario /{" "}
+            <Link
+              href={"create-user"}
+              className=" hover:text-blue-600 hover:underline"
+            >
+              Crear
+            </Link>
           </label>
         </div>
-        <button
-          type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Log In
-        </button>
+        <div className=" text-center">
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-[200px] px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Log In
+          </button>
+        </div>
       </form>
     </div>
   );
